e7038ea34a1ac2a82728dbe1b54063195172bea0
Paulo Henrique Pfitzner <cientistaphp@gmail.com>
2026-03-19 15:22:59 -0300

    fix: api link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-      baseURL: 'http://https://tweetify-production.up.railway.app' 
+  baseURL: 'https://tweetify-production.up.railway.app',
 });
 
 export default api;
