39f675a742d5cbc8234df0b06033b4b8846a79bd
Paulo Henrique Pfitzner <cientistaphp@gmail.com>
2026-03-19 15:12:02 -0300

    fix: update Timeline completo (Railway)

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,54 +1,61 @@
-import  React, { Component } from 'react';
+import React, { Component } from 'react';
 import api from '../services/api';
 import socketIOClient from "socket.io-client";
 
 import twitterLogo from '../twitter.svg';
-import './Timeline.css'
+import './Timeline.css';
 
 import Tweet from '../components/Tweet';
 
 export default class Timeline extends Component {
     state = {
         tweets: [],
-       newTweet: '',  
-    }; 
+        newTweet: '',
+    };
 
-    async componentDidMount(){
+    async componentDidMount() {
         this.subscribeToEvents();
-        const response = await api.get('tweets');
 
-        this.setState({tweets: response.data })
-
+        try {
+            const response = await api.get('/tweets');
+            this.setState({ tweets: response.data });
+        } catch (err) {
+            console.log("Erro ao buscar tweets:", err);
+        }
     }
 
-    
+    subscribeToEvents = () => {
+        const io = socketIOClient('https://tweetify-production.up.railway.app');
 
-subscribeToEvents = () => {
-    const io = socketIOClient('https://tweetify-production.up.railway.app');
-
-    io.on('tweet', data => {
-        this.setState({ tweets: [data, ...this.state.tweets] });
-    });
-
-    io.on('like', data => {
-        this.setState({
-            tweets: this.state.tweets.map(tweet =>
-                tweet._id === data._id ? data : tweet
-            )
+        io.on('tweet', data => {
+            this.setState(prevState => ({
+                tweets: [data, ...prevState.tweets]
+            }));
         });
-    });
-};
 
-    handleNewTweet = async e => {
-        if (e.keyCode !== 13) return;
+        io.on('like', data => {
+            this.setState(prevState => ({
+                tweets: prevState.tweets.map(tweet =>
+                    tweet._id === data._id ? data : tweet
+                )
+            }));
+        });
+    };
+
+    handleNewTweet = async (e) => {
+        if (e.key !== 'Enter') return;
+
+        e.preventDefault(); // 🔥 evita quebrar linha
 
         const content = this.state.newTweet;
         const author = localStorage.getItem("@GoTwitter:username");
 
-        await api.post('tweets', {content, author});
-
-        this.setState({ newTweet: ''});
-
+        try {
+            await api.post('/tweets', { content, author });
+            this.setState({ newTweet: '' });
+        } catch (err) {
+            console.log("Erro ao enviar tweet:", err);
+        }
     };
 
     handleInputChange = e => {
@@ -57,23 +64,24 @@ subscribeToEvents = () => {
 
     render() {
         return (
-           <div className="timeline-wrapper">
-             <img height={24} src={twitterLogo} alt="GoTwitter" />
-             <form>
-                <textarea
-                  value={this.state.newTweet}
-                  onChange={this.handleInputChange}
-                  onKeyDown={this.handleNewTweet}
-                  placeholder="O que está acontecendo?"
-                  />
-             </form>
-           <ul className="tweet-list">
-            { this.state.tweets.map(tweet => (
-                <Tweet key={tweet._id} tweet={tweet}/>
-            ))} 
-            </ul>
-          </div>
-          
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontecendo?"
+                    />
+                </form>
+
+                <ul className="tweet-list">
+                    {this.state.tweets.map(tweet => (
+                        <Tweet key={tweet._id} tweet={tweet} />
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
